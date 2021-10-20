b2679ff92531362793cd5794687a1aede1127137
Juanma L <juanmalubian@gmail.com>
2021-10-20 12:24:48 +0200

    Borradas clases innecesarias

--- src/main/java/com/nttdata/bootcamp/PruebaStarterApplication.java
+++ src/main/java/com/nttdata/bootcamp/PruebaStarterApplication.java
@@ -1,15 +1,20 @@
 package com.nttdata.bootcamp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class StarterApplication {
 
+@SpringBootApplication
+public class PruebaStarterApplication {
+
+	@Autowired 
+	private Saludo s;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(StarterApplication.class, args);
-//		Saludo saludo = new Saludo("Castellano");
-//		System.out.println(saludo.mensajeSaludo());
+		SpringApplication.run(PruebaStarterApplication.class, args);
+		
+		
 	}
 
 }
---
+++ src/main/java/com/nttdata/bootcamp/controller/Controlador.java
@@ -0,0 +1,23 @@
+package com.nttdata.bootcamp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nttdata.bootcamp.Saludo;
+
+
+@RestController
+public class Controlador {
+	
+	@Autowired
+	private Saludo saludo;
+	
+	@GetMapping("/saludo")
+	public String getSaludo() {		
+		
+		return saludo.mensajeSaludo();
+	}
+}
